7ab10f452f7a8aec5c0e5f7b17f44b813dc0797d
dewey92 <dewey992@gmail.com>
2020-01-19 02:42:41 +0100

    add dropdown block

--- src/Components/Dropdown.purs
+++ src/Components/Dropdown.purs
@@ -1,37 +1,24 @@
 module Hasyr.Components.Dropdown where
 
-import Prelude
-
-import Data.Symbol (SProxy(..))
-import Halogen (Component, defaultEval, mkComponent, mkEval)
 import Halogen.HTML as H
 import Hasyr.Utils.HTML (className)
 
-type Props ca cs m =
-  { dropdownTrigger :: H.ComponentHTML ca cs m
-  , dropdownContent :: H.ComponentHTML ca cs m
+type Props w i =
+  { trigger :: H.HTML w i
+  , content :: H.HTML w i
   }
 
-component :: ∀ q ca cs m. Component H.HTML q (Props ca cs m) {} m
-component = mkComponent
-  { initialState: identity
-  , eval: mkEval defaultEval
-  , render
-  } where
-
-  render state =
-    H.div [className "dropdown is-hoverable"] [
-      H.div [className "dropdown-trigger"] [
-        state.dropdownTrigger
-      ],
-      H.div [className "dropdown-menu"] [
-        H.div [className "dropdown-content"] [
-          H.div [className "dropdown-item"] [
-            state.dropdownContent
-          ]
+block :: ∀ w i. Props w i -> H.HTML w i
+block props =
+  H.div [className "dropdown is-hoverable is-right"] [
+    H.div [className "dropdown-trigger"] [
+      props.trigger
+    ],
+    H.div [className "dropdown-menu"] [
+      H.div [className "dropdown-content"] [
+        H.div [className "dropdown-item"] [
+          props.content
         ]
       ]
     ]
-
-_dt = SProxy :: SProxy "dropdownTrigger"
-_dc = SProxy :: SProxy "dropdownContent"
+  ]
